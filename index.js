96755ffe9443639d7391ab034bb2419fe5cb71db
dead-horse <dead_horse@qq.com>
2020-03-11 20:43:26 +0800

    fix: empty input empty output

--- index.js
+++ index.js
@@ -9,18 +9,24 @@ module.exports = function (funcs, n) {
   if (funcs.some(func => func && typeof func.then === 'function')) n = funcs.length;
 
   return new Promise(resolve => {
+    if (funcs.length === 0) return resolve([]);
+
     const results = [];
     let index = 0;
     let finish = 0;
-
     for (let i = 0; i < n; i++) {
       run();
     }
 
     function run() {
       const i = index++;
+      if (i >= funcs.length) return;
       let ins = funcs[i];
-      if (!ins) return;
+      if (!ins) {
+        results[i] = undefined;
+        if (++finish >= funcs.length) return resolve(results);
+        return;
+      }
 
       if (typeof ins === 'function') ins = ins();
       if (ins && typeof ins.next === 'function' && typeof ins.throw === 'function') ins = require('co')(ins);
